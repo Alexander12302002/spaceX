d852fa675dc2956e848bbef80eb167a1e544662d
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-04-30 17:02:07 -0500

    feat: :sparkles: factoring of code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-import { getAllRockets } from "./modules/rockets.js";
+import { 
+    getMasaRockets,
+    getRocketsPayLoadWeights,
+    getHeightRockets,
+    getDiameterRockets,
+    getDiameterCompositeRockets,
+    getHeightCompositeRockets
+} from "./modules/rockets.js";
 import { 
     progressRocketWeight, 
     progressPayloadWeights, 
@@ -10,17 +17,27 @@ import {
 
 
 let information__2 = document.querySelector("#information__2");
-let dataRockets = await getAllRockets();
-let Totales = dataRockets.pop()
+let dataMasaRockets = await getMasaRockets();
+let dataRocketsPayLoadWeight = await getRocketsPayLoadWeights();
+let dataHeightRockets = await getHeightRockets();
+let dataDiameterRockest = await getDiameterRockets()
+let dataDiameterCompositeRocket = await getDiameterCompositeRockets()
+let dataHeightCompositeRockets = await getHeightCompositeRockets()
 
-let [Rockets1, Rockets2, Rockets3, Rockets4] = dataRockets; 
 
-console.log(dataRockets);
-console.log(Totales);
+let TotalMasaRocket = dataMasaRockets.pop()
+let TotalPayLoadWeight = dataRocketsPayLoadWeight.pop()
+let TotalHeightRocket = dataHeightRockets.pop()
+let TotalDiameterRockest = dataDiameterRockest.pop()
+let TotalDiameterCompositeRockest = dataDiameterCompositeRocket.pop()
+let TotalHeightCompositeRockets = dataHeightCompositeRockets.pop()
 
-information__2.append(...progressRocketWeight(Totales.kg_max, Rockets3))
-information__2.append(...progressPayloadWeights(Totales.payload_weights, Rockets3))
-information__2.append(...progressHeightRocket(Totales.height, Rockets3))
-information__2.append(...progressDiameterRocket(Totales.diameter, Rockets3))
-information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rockets3))
-information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rockets3))
+let [Rockets1, Rockets2, Rockets3, Rockets4] = dataMasaRockets; 
+
+
+information__2.append(...progressRocketWeight(TotalMasaRocket.kg_max, Rockets3))
+information__2.append(...progressPayloadWeights(TotalPayLoadWeight.payload_weights, Rockets3))
+information__2.append(...progressHeightRocket(TotalHeightRocket.height, Rockets3))
+information__2.append(...progressDiameterRocket(TotalDiameterRockest.diameter, Rockets3))
+information__2.append(...progressSecondStageDiameterRocket(TotalDiameterCompositeRockest.composite_diameter, Rockets3))
+information__2.append(...progressSecondStageHeightRocket(TotalHeightCompositeRockets.composite_height, Rockets3))
--- js/modules/rockets.js
+++ js/modules/rockets.js
@@ -1,34 +1,93 @@
-export const getAllRockets = async ()=>{
+export const getMasaRockets = async ()=>{
     let res = await fetch("https://api.spacexdata.com/v4/rockets")
     let data = await res.json();
-    let listMasa = [], 
-        listPayloadWeights = [], 
-        heightRocket = [], 
-        listDiameter = [], 
-        listaDiameterComposite = [],
-        listaHeightComposite = [];
+    let listMasa = []; 
 
     data.forEach((val, id) => {
         listMasa.push(val.mass.kg);
-        listPayloadWeights.push(...val.payload_weights)
-        heightRocket.push(val.height)
-        listDiameter.push(val.diameter)
-        listaDiameterComposite.push(val.second_stage.payloads.composite_fairing.diameter)
-        listaHeightComposite.push(val.second_stage.payloads.composite_fairing.height)
     });
     
     listMasa.sort((a,b) => b - a)
-    listPayloadWeights.sort((a,b) => b.kg - a.kg)
-    heightRocket.sort((a,b) => b.meters - a.meters)
-    listDiameter.sort((a,b) => b.meters - a.meters)
-    listaDiameterComposite.sort((a,b) => b.meters - a.meters)
-    listaHeightComposite.sort((a,b) => b.meters - a.meters)
     data.push({
         kg_max: listMasa.shift(), 
-        payload_weights: listPayloadWeights.shift().kg,
-        height: heightRocket.shift().meters,
-        diameter: listDiameter.shift().meters,
-        composite_diameter : listaDiameterComposite.shift().meters,
+    });
+    return data;
+}
+
+export const getRocketsPayLoadWeights = async ()=>{
+    let res = await fetch("https://api.spacexdata.com/v4/rockets");
+    let data = await res.json();
+    let payLoadWeigth = [];
+
+    data.forEach((val) =>{
+        payLoadWeigth.push(...val.payload_weights)
+    });
+
+    payLoadWeigth.sort((a,b)=> b.kg - a.kg)
+    data.push({
+        payload_weights: payLoadWeigth.shift().kg,
+    });
+    return data;
+}
+
+export const getHeightRockets = async ()=>{
+    let res = await fetch("https://api.spacexdata.com/v4/rockets");
+    let data = await res.json();
+    let heightRockets = [];
+
+    data.forEach((val) =>{
+        heightRockets.push(val.height)
+    })
+
+    heightRockets.sort((a,b) => b.meters - a.meters)
+    data.push({
+        height: heightRockets.shift().meters
+    });
+    return data;
+}
+
+export const getDiameterRockets = async()=>{
+    let res = await fetch("https://api.spacexdata.com/v4/rockets");
+    let data = await res.json();
+    let diameterRocket = [];
+
+    data.forEach((val) =>{
+        diameterRocket.push(val.diameter)
+    });
+
+    diameterRocket.sort((a,b) => b.meters - a.meters)
+    data.push({
+        diameter: diameterRocket.shift().meters
+    })
+
+    return data;
+}
+
+export const getDiameterCompositeRockets = async()=>{
+    let res = await fetch("https://api.spacexdata.com/v4/rockets")
+    let data = await res.json();
+    let listaDiameterComposite = [];
+
+    data.forEach((val) =>{
+        listaDiameterComposite.push(val.second_stage.payloads.composite_fairing.diameter)
+    });
+    listaDiameterComposite.sort((a,b) => b.meters - a.meters)
+    data.push({
+        composite_diameter: listaDiameterComposite.shift().meters
+    });
+    return data
+}
+
+export const getHeightCompositeRockets = async()=>{
+    let res = await fetch("https://api.spacexdata.com/v4/rockets")
+    let data = await res.json();
+    let listaHeightComposite = [];
+
+    data.forEach((val) =>{
+        listaHeightComposite.push(val.second_stage.payloads.composite_fairing.height)
+    });
+    listaHeightComposite.sort((a,b) => b.meters - a.meters)
+    data.push({
         composite_height: listaHeightComposite.shift().meters
     });
     return data;
